5506f3e503740c225384dc58cbc702e6f1e9bb0e
terrajanis <53621804+terrajanis@users.noreply.github.com>
2022-04-09 13:04:08 +0300

    add auth to system props

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gorest-tests
+
+To run tests with your own token mvn test -Dtoken={your token} -DsuiteXmlFile=regress.xml
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -7,17 +7,15 @@ import java.util.Properties;
 public class BaseTest {
 
     protected Properties properties = new Properties();
-    protected Properties authProperties = new Properties();
 
     {
         try {
             properties.load(new FileInputStream("src/test/resources/application.properties"));
-            authProperties.load(new FileInputStream("src/test/resources/auth.properties"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     protected final String BASE_URL = properties.getProperty("baseUrl");
-    protected final String BASE_TOKEN = authProperties.getProperty("token");
+    protected final String BASE_TOKEN = System.getProperty("token");
 }
